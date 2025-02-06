7e433cdd0c7fa5c8b23b68aaf858788af5ee3fca
HiHi <110230054+0Jmins0@users.noreply.github.com>
2025-02-07 00:56:53 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -13,31 +13,45 @@ import matplotlib.pyplot as plt
 
 # 定义命令行参数解析器
 parser = argparse.ArgumentParser(description="Train MVCNN_CLIP model")
-parser.add_argument("--batch_size", type=int, default=8, help="Batch size for training (default: 10)")
-parser.add_argument("--num_epochs", type=int, default=20, help="Number of epochs to train (default: 1)")
+parser.add_argument("--batch_size", type=int, default=12, help="Batch size for training (default: 10)")
+parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs to train (default: 1)")
 parser.add_argument("--lr", type=float, default=0.0001, help="Learning rate (default: 0.001)")
 parser.add_argument("--margin", type=float, default=1.0, help="Margin for triplet loss (default: 1.0)")
-parser.add_argument("--num_views", type=int, default=8, help="Number of views for MVCNN (default: 10)")
+parser.add_argument("--num_views", type=int, default=15, help="Number of views for MVCNN (default: 10)")
 parser.add_argument("--data_root", type=str, default="../data/ModelNet_random_30_final/DS/train", help="Root directory of the dataset (default: ../data/ModelNet_random_30_final/DS/train)")
 parser.add_argument("--model_path", type=str, default="../models/train_models/base/mvcnn_clip_5.pth", help="Path to save the trained model (default: ../models/train_models/base/mvcnn_clip_01.pth)")
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# transform = transforms.Compose([
+#     transforms.Resize((256, 256)),  # 先将图像调整到更大的尺寸
+#     transforms.RandomResizedCrop(224),  # 随机裁剪到目标尺寸
+#     transforms.RandomHorizontalFlip(p=0.5),  # 随机水平翻转，概率为0.5
+#     transforms.RandomVerticalFlip(p=0.5),  # 随机垂直翻转，概率为0.5
+#     transforms.RandomRotation(degrees=15),  # 随机旋转，角度范围为±15度
+#     transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 2.0)),  # 随机高斯模糊
+#     transforms.ToTensor(),  # 转换为张量
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 归一化
+# ])
+
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((224, 224)),  # 直接调整到 CLIP 的输入尺寸
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])    
+    transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])  # CLIP 专用参数
 ])
 
 print("loading train data.......")
-train_dataset = MultiViewDataset(root_dir="../data/ModelNet_random_30_final/DS/train",transform=transform)
+train_dataset = MultiViewDataset(root_dir="../data/ModelNet_random_30_final/DS/train",transform=transform, num_views=args.num_views)
 train_loader = DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True, drop_last=True)
 
 print("finished loading train data.......")
-model = MVCNN_CLIP(num_views = args.batch_size).to(device)
-criterion = TripletLoss(margin = 1.0)
-optimizer = optim.Adam(model.parameters(),lr = args.lr)
+model = MVCNN_CLIP(num_views = args.num_views).to(device)
+criterion = TripletLoss(margin = 0.5)
+optimizer = optim.Adam([
+    {"params": model.net_1.parameters(), "lr": 1e-6},  # 主干网络低学习率
+    # {"params": model.net_2.parameters(), "lr": 1e-6}   # 新增层高学习率
+])
 
 # 确保保存模型的目录存在
 os.makedirs(os.path.dirname(args.model_path), exist_ok=True)
@@ -61,22 +75,51 @@ losses = []
 model.train()
 for epoch in range(start_epoch + 1, num_epochs):
     epoch_loss = 0.0
-    for anchor, positive, negative in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}", unit="batch"):
-        anchor, positive, negative = anchor.to(device), positive.to(device), negative.to(device)
+    i = 0
+    for anchor_images, positive_images, negative_images in tqdm(train_loader, desc=f"Epoch {epoch}/{num_epochs}", unit="batch"):
+        i += 1
+        anchor_images = anchor_images.view(-1, *anchor_images.shape[-3:])  # (batch_size * num_views, C, H, W)
+        positive_images = positive_images.view(-1, *positive_images.shape[-3:])  # (batch_size * num_views, C, H, W)
+        negative_images = negative_images.view(-1, *negative_images.shape[-3:])  # (batch_size * num_views, C, H, W)
+
+        anchor_images, positive_images, negative_images = anchor_images.to(device), positive_images.to(device), negative_images.to(device)
 
         optimizer.zero_grad()
-        anchor_features = model(anchor)
-        positive_features = model(positive)
-        negative_features = model(negative)
+        anchor_features = model(anchor_images)
+        positive_features = model(positive_images)
+        negative_features = model(negative_images)
 
         loss = criterion(anchor_features, positive_features, negative_features)
         loss.backward()
+
+        # 打印梯度
+        # for name, param in model.named_parameters():
+        #     if param.grad is not None:
+        #         print(f"Gradient of {name}: {param.grad.norm().item():.4f}")
+
         optimizer.step()
         epoch_loss += loss.item()
+
+        losses.append(loss.item())
+        if(i % 20 == 0):
+            # 确保存储路径存在
+            save_path = f"../models/train_models/base/pics/loss_curve_epoch_{epoch}_batch_{i}.png"
+            os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+            plt.figure(figsize=(10, 5))
+            plt.plot(range(1, len(losses) + 1), losses, label="Training Loss")
+            plt.xlabel("Batch")
+            plt.ylabel("Loss")
+            plt.title(f"Training Loss Over Batches (Epoch {epoch})")
+            plt.legend()
+            plt.grid(True)
+            plt.savefig(save_path)
+
+
     
     epoch_loss /= len(train_loader)
     losses.append(epoch_loss)
-    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}")
+    print(f"Epoch [{epoch}/{num_epochs}], Loss: {epoch_loss:.4f}")
 
     # 保存模型
     torch.save({
