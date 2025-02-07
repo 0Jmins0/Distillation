2b5c8349f5268c359e2bb16d84e312d02882152e
HiHi <110230054+0Jmins0@users.noreply.github.com>
2025-02-07 15:28:29 +0800

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -14,18 +14,19 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])    
 ])
 
-test_dataset = TestDataset(root_dir="../data/ModelNet_random_30_final/DS/retrieval",transform=transform)
+test_dataset = TestDataset(root_dir="../data/ModelNet_random_30_final/DU/retrieval",transform=transform)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True, drop_last=True)
 
+query_image = Image.open("../data/ModelNet_random_30_final/DU/retrieval/sofa/sofa_0186/sofa_0186.2.png").convert("RGB")
+
 model = MVCNN_CLIP(num_views=1).to(device)
-model.load_state_dict(torch.load("../models/train_models/base/mvcnn_clip_01.pth"))
+model.load_state_dict(torch.load("../models/train_models/base/mvcnn_clip_9.pth")['model_state_dict'])
 model.eval()
 
 features, image_paths = extract_features(model, test_loader, device)
 
-query_image = Image.open("../data/ModelNet_random_30_final/DS/retrieval/bathtub/bathtub_0008/bathtub_0008.1.png").convert("RGB")
 # query_image = transform(query_image).unsqueeze(0).unsqueeze(1).to(device) 
-results = retrieve_images(query_image, features, image_paths, model, top_k=5)
+results = retrieve_images(query_image, features, image_paths, model, top_k=10)
 
 for path, similarity in results:
     print(f"Image: {path}, Similarity: {similarity:.4f}")
